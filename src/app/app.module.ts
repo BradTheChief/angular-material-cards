f60e28484b815af8597e8224f4ecf9a7d9454ce5
BradTheChief <cuse0314@gmail.com>
2022-04-24 11:54:20 -0400

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-import { MatCardModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
