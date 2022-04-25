98a0848050823a939482659af27fd4feb2c333db
BradTheChief <cuse0314@gmail.com>
2022-04-24 21:56:16 -0400

    5 files modified, 1 added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ import { TestComponent } from './test/test.component';
 const routes: Routes = [
   { path: '', redirectTo: '/register', pathMatch: 'full' },
   { path: 'register', component: RegisterComponent },
-  { path: 'test', component: TestComponent },
+  { path: 'user', component: TestComponent },
 ];
 
 @NgModule({
--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Test } from './test';
 
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.css'],
 })
-export class TestComponent {}
+
+export class TestComponent { }
