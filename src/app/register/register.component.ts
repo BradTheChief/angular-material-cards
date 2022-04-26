847c238e7dac5e483cb5971fd444723168d53ebd
BradTheChief <cuse0314@gmail.com>
2022-04-25 20:50:54 -0400

    1 file modified

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,24 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { User } from '../user/user'
+import { User } from '../user/user';
 import { RegisterService } from '../register.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
 })
-
 export class RegisterComponent {
+  constructor(private http: HttpClient) {}
+  
   submitted = false;
 
-  userModel = new User('', '', '', '',  '');
+  userModel = new User('', '', '', '', '');
 
-
-onSubmit(){
-  
-  }
-  
-  
- }
- 
+  onSubmit() {}
+}
